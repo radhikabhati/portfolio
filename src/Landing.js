9f962e8fb215970f9700e4625e3c31bcd9892c00
Radhika Bhati <rbhati@asu.edu>
2024-09-22 16:03:43 -0700

    typo fix

--- src/Landing.js
+++ src/Landing.js
@@ -7,7 +7,7 @@ function Landing() {
     
 
      <div className="subsection-holder">
-      <div className='about'>I am a full stack engineer. I like painting, gardening, photography, making art, astrology, geeking out on web designers pages , taking long walks while spacing out and spending all my time with my dogs. This is my work-in-progress portfolio website which will include my professional details but alos depict my interests and is made just for fun :)</div>
+      <div className='about'>I am a full stack engineer. I like painting, gardening, photography, making art, astrology, spiralling down web design rabit holes, taking long walks as I space out and spending all my time with my dogs. This is my work-in-progress portfolio website which will include my professional details but also depict my interests and is made just for fun :)</div>
       <div className='sub'>Fun/Interests</div>
       <div className='sub'>Scratchpad</div>
       <div className='sub'>Reading list</div>
