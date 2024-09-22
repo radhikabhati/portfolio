060840946727ac651790aaec1172e005f090de3f
Radhika Bhati <rbhati@asu.edu>
2024-09-21 17:30:15 -0700

    landing placeholders

--- src/Landing.js
+++ src/Landing.js
@@ -4,14 +4,14 @@ function Landing() {
   return (
     <div className="content">
      <h1>Hi, my name is Radhika.. </h1>
-     <h3>This is my work-in-progress portfolio website.</h3>
-    <hr/>
+    
+
      <div className="subsection-holder">
-      <h4>TODO</h4>
-      <h5>background</h5>
-      <h5>interests</h5>
-      <h5>Currently reading</h5>
-      <h5>idea scratch pad</h5>
+      <div className='about'>I am a full stack engineer. I like painting, gardening, photography, making art, astrology, geeking out on web designers pages , taking long walks while spacing out and spending all my time with my dogs. This is my work-in-progress portfolio website which will include my professional details but alos depict my interests and is made just for fun :)</div>
+      <div className='sub'>Fun/Interests</div>
+      <div className='sub'>Scratchpad</div>
+      <div className='sub'>Reading list</div>
+      <div className='sub'>placeholder subsection</div>
       
 
      </div>
