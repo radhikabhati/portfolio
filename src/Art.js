43f16b584285effb911ea4e12bad17dd39187315
Radhika Bhati <rbhati@asu.edu>
2024-12-21 22:44:35 -0500

    cleaning art oage

--- src/Art.js
+++ src/Art.js
@@ -4,15 +4,15 @@ import "./styles/Art.css"
 function Art() {
   return (
     <div className="Art">
-      <h1>bats_022</h1>
+     
       <div style={{ width: '100%', height: '1200px', overflow: 'hidden', border: '1px solid #ddd', borderRadius: '10px' }}>
         <iframe 
           title="bats_022"
           src="https://www.instagram.com/bats_022/embed"
           width="100%" 
-          height="10000" 
+           height="1210" 
           frameBorder="0" 
-          scrolling="no" 
+          scrolling="yes" 
           allowTransparency="true" 
           allow="encrypted-media"
         ></iframe>
